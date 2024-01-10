71dd43bf1e10f78b6b3144b0c04ef96f08ab5faf
татьяна090975 <tatyana090975@gmail.com>
2024-01-10 13:42:30 +0400

    Задание 5.1.5

--- Program.cs
+++ Program.cs
@@ -8,22 +8,58 @@ namespace Mod5MethodsInC
 {
     internal class Program
     {
+        static string ShowColor(string color)
+        {
+            //Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            //var color = Console.ReadLine();
+
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+               
+            }
+            return color;
+        }
+
         static void Main(string[] args)
         {
-            (string Name, string[] Dishes) User;
-            Console.WriteLine("Введите имя пользователя: ");
-            User.Name = Console.ReadLine();
-            Console.WriteLine("Введите пять любимых блюд: ");
-            string[] Dishes = new string[5];
-            for (int i = 0; i < 5; i++)
+            string[] favcolors = new string[3];
+            for (int i = 0; i < 3; i++)
             {
-                Dishes[i] = Console.ReadLine();
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+                favcolors[i] = Console.ReadLine();
+                ShowColor(favcolors[i]);
             }
-            /*
-            for (int i = 0; i < 5; i++)
+            Console.WriteLine("Ваши любимые цвета:");
+            for (int k = 0; k < favcolors.Length; k++)
             {
-                Console.WriteLine(Dishes[i]);
-            }*/
+                Console.WriteLine(favcolors[k]);
+            }
+            
             Console.ReadKey();
         }
     }

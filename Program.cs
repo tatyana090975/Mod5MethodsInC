344e527e9e48531b37c28969c558200811190da5
татьяна090975 <tatyana090975@gmail.com>
2024-01-10 15:06:45 +0400

    Задание 5.1.6

--- Program.cs
+++ Program.cs
@@ -44,22 +44,46 @@ namespace Mod5MethodsInC
             }
             return color;
         }
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
 
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
-            string[] favcolors = new string[3];
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-                favcolors[i] = Console.ReadLine();
-                ShowColor(favcolors[i]);
-            }
-            Console.WriteLine("Ваши любимые цвета:");
-            foreach (var color in favcolors)
-            {
-                Console.WriteLine(color);
-            }
+            int[] arr = GetArrayFromConsole();
+            int temp;
+
             
+                
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item);
+            }
+
+
             Console.ReadKey();
         }
     }
